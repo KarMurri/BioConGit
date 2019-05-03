c56ba5d1e21ad87f34f177d209f37a8a602660ec
Karen Murrieta <karenmurrieta@live.com>
2019-05-02 23:14:38 -0500

    Script completo V1

--- js/facebookSDK.js
+++ js/facebookSDK.js
@@ -8,3 +8,37 @@ window.fbAsyncInit = function() {
       version          : 'v3.3'
     });
 };
+
+FB.login(function(response) {
+  checkLoginState();
+}, {scope: 'email'});
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else {
+      // The person is not logged into your app or we are unable to tell.
+      alert('Por favor inicie sesi&oacute');
+    }
+}
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name + ', email: '+ response.email);
+      alert('Successful login for: ' + response.name + ', email: '+ response.email);
+    });
+}
