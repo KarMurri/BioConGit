7035f81f8526b626efb65a09f4614314ef3fe1c5
Karen Murrieta <karenmurrieta@live.com>
2019-05-02 23:52:11 -0500

    Script regreso V1

--- js/facebookSDK.js
+++ js/facebookSDK.js
@@ -8,29 +8,3 @@ window.fbAsyncInit = function() {
       version          : 'v3.3'
     });
 };
-
-var FB = window.FB;
-
-FB.login(function(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else {
-      // The person is not logged into your app or we are unable to tell.
-      alert('Por favor inicie sesi&oacute');
-    }
-}, {scope: 'email',return_scopes: true});
-
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name + ', email: '+ response.email);
-      alert('Successful login for: ' + response.name + ', email: '+ response.email);
-    });
-}
