00622c9e5cc4e532a03bef9b585491fa21289551
Karen Murrieta <karenmurrieta@live.com>
2019-05-02 21:27:26 -0500

    Se agrega script de SDK

--- js/facebookSDK.js
+++ js/facebookSDK.js
@@ -1,1 +1,10 @@
 //aquí empieza SDK de facebook
+console.log('entre a SDK');
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '2610429415851014',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.3'
+    });
+};
